c77831fad767ec6ede217506f62afc11e4392d4a
Franco Alla <frannelfran@gmail.com>
2025-03-23 16:30:09 +0000

    feat: Implementación de los getters

--- src/zona/zona.h
+++ src/zona/zona.h
@@ -11,10 +11,15 @@ using namespace std;
 class Zona {
   public:
   // Constructor de la clase
-  Zona(const string& id, const pair<int, int>& posicion, const int capacidad) : id_(id), posicion_(posicion), capacidad_(capacidad) {};
+  Zona(const string& id, const pair<double, double>& posicion, const double& contenido) : id_(id), posicion_(posicion), contenido_(contenido) {};
+
+  // Getters
+  inline string getId() const { return id_; }
+  inline pair<double, double> getPosicion() const { return posicion_; }
+  inline double getContenido() const { return contenido_; }
 
   private:
   string id_;
-  pair<int, int> posicion_;
-  int capacidad_;
+  pair<double, double> posicion_;
+  double contenido_;
 };
