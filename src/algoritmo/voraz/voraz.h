2db68e2df3259aa5a1c17d2c8664c5d73c41d644
Franco Alla <frannelfran@gmail.com>
2025-03-26 17:26:44 +0000

    feat: Creación de la clase para construir las rutas de los vehículos de recolección

---
+++ src/algoritmo/voraz/voraz.h
@@ -0,0 +1,15 @@
+/**
+ * @class Algortimo Voraz para determinar la ruta de los vehículos de recolección
+ */
+
+#pragma once
+#include "algoritmo.h"
+
+class Voraz : public Algoritmo {
+  public:
+  // Constructor de la clase
+  Voraz(const Tools& datos) : Algoritmo(datos) {};
+
+  // Métodos de la clase
+  vector<Vehiculo> ejecutar() override;
+};
