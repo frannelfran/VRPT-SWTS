a99173ca61bd1dadf1897f4e14ecd82d00e9171d
Franco Alla <frannelfran@gmail.com>
2025-03-26 16:57:34 +0000

    feat: Implementación de los métodos de la clase vehículo

---
+++ src/vehiculo/vehiculo.cc
@@ -0,0 +1,47 @@
+#include "vehiculo.h"
+
+/**
+ * @brief Constructor de la clase
+ * @param capacidad Capacidad del vehículo
+ * @param velocidad Velocidad del vehículo
+ * @param posicion Posición inicial del vehículo
+ * @param duracion Duración máxima que puede estar el vehiculo en funcionamiento
+ * @param zonas Zonas que tiene que visitar el vehículo
+ * @return Vehiculo
+ */
+Vehiculo::Vehiculo(const int& capacidad, const int& velocidad, const pair<int, int>& posicion, const int& duracion, const vector<Zona>& zonas) {
+  capacidad_ = capacidad;
+  velocidad_ = velocidad;
+  posicion_ = posicion;
+  duracion_ = duracion;
+  zonas_ = zonas;
+}
+
+/**
+ * @brief Método para llenar el vehículo
+ * @param cantidad Cantidad de residuos que se van a añadir al vehículo
+ * @return void
+ */
+void Vehiculo::llenarVehiculo(double cantidad) {
+  cantidad_ += cantidad;
+}
+
+/**
+ * @brief Método para vaciar el vehículo
+ * @param zona Zona en la que se va a vaciar el vehículo
+ * @return void
+ */
+void Vehiculo::vaciarVehiculo(Zona& zona) {
+  zona.setContenido(this->cantidad_);
+  this->cantidad_ = 0.0;
+}
+
+/**
+ * @brief Método para mover el vehículo a una zona
+ * @param zona Zona a la que se va a mover el vehículo
+ * @return void
+ */
+void Vehiculo::moverVehiculo(const Zona& zona) {
+  posicion_ = zona.getPosicion();
+  zonasVisitadas_.push_back(zona);
+}
--- src/vehiculo/vehiculo.h
+++ src/vehiculo/vehiculo.h
@@ -5,17 +5,29 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+#include "../zona/zona.h"
 
 using namespace std;
 
 class Vehiculo {
   // Constructor de la clase
   public:
-  Vehiculo(const int& capacidad, const int& velocidad, const pair<int, int>& posicion, const int& duracion);
+  Vehiculo(const int& capacidad, const int& velocidad, const pair<int, int>& posicion, const int& duracion, const vector<Zona>& zonas);
 
+  // Métodos de la clase
+  void llenarVehiculo(double cantidad);
+  void vaciarVehiculo(Zona& zona);
+  void moverVehiculo(const Zona& zona);
+  
   private:
+  // Valores que dependen para que el vehículo siga funcionando
   int capacidad_;
-  int velocidad_;
-  pair<int, int> posicion_;
   int duracion_;
+  // Datos importantes para el vehículo
+  double cantidad_ = 0.0;
+  int velocidad_;
+  pair<int, int> posicion_; // Posición actual del vehículo
+  vector<Zona> zonasVisitadas_; // Zonas que ha visitado el vehículo
+  vector<Zona> zonas_; // Conjunto de zonas que tiene que visitar el vehículo
 };
