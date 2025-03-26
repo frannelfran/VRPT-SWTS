2c503b56760f6a0de525eafd6351fdc80dafca08
Franco Alla <frannelfran@gmail.com>
2025-03-26 16:58:35 +0000

    feat: Creación del fichero main

---
+++ src/main.cc
@@ -0,0 +1,28 @@
+/**
+ * @file main.cc
+ * @brief Función principal del programa
+ */
+#include "tools/tools.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    cerr << "Error: Número de argumentos incorrecto" << endl;
+    cerr << "Uso: " << argv[0] << " <nombre_fichero>" << endl;
+    exit(1);
+  }
+  string fileName = argv[1];
+  try {
+    Tools tools = readData(fileName);
+    vector<Zona> zonas = tools.zonas;
+    // Muestro las zonas
+    cout << "Zonas:" << endl;
+    for (int i = 0; i < zonas.size(); i++) {
+      cout << zonas[i].getId() << " " << zonas[i].getPosicion().first << " " << zonas[i].getPosicion().second << " " << zonas[i].getContenido() << endl;
+    }
+  } catch (const invalid_argument& e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+  
+  return 0;
+}
